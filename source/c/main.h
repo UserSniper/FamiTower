07600171d97f8191ae4545d79492786b66c17714
UserSniper <vruh.69@gmail.com>
2023-04-30 21:28:48 -0400

    omfg how do i fix "duplicate external identifier"

---
+++ source/c/main.h
@@ -0,0 +1,67 @@
+// the background
+#include "c1.c";
+
+
+
+//
+// Global Variables (zeropage) 
+// Small, frequently-used variables should go in this space. There are only around 250 bytes to go around, so choose wisely!
+//
+#pragma bss-name(push, "ZEROPAGE")
+    unsigned char i;
+    unsigned char pad1;
+    unsigned char collision;
+    unsigned char collision_L;
+    unsigned char collision_R;
+    unsigned char collision_U;
+    unsigned char collision_D;
+    unsigned char which_bg;
+    const unsigned char * p_maps;
+    unsigned char coordinates;
+    unsigned char temp1;
+    unsigned char temp2;
+    unsigned char temp3;
+    unsigned char temp4;
+    unsigned char temp_x;
+    unsigned char temp_y;
+
+
+
+
+#pragma bss-name(pop)
+//
+// Normal Variables
+// You can define larger, less frequently accessed variables here. You have about 1.5k to work with.
+//
+// EXAMPLE: 
+// unsigned char myBigBufferArray[32];
+unsigned char testVariable;
+unsigned char c_map[240];
+
+
+
+
+//
+// Constant variables
+// Anything with const in front of it will go into write-only prg instead of the very limited ram we have.
+//
+const unsigned char * const Collision_Maps[] = {c1};
+
+const unsigned char welcomeMessage[] = "Make something unique!";
+
+// Color palette for the screen to use
+const unsigned char palette[32]={ 
+    0x0f,0x03,0x13,0x24,0x0f,0x00,0x10,0x30,0x0f,0x0f,0x0f,0x0f,0x0f,0x0f,0x0f,0x0f,
+    0x0f,0x0f,0x37,0x30,0x0f,0x0f,0x0f,0x0f,0x0f,0x0f,0x0f,0x0f,0x0f,0x0f,0x0f,0x0f
+};
+
+
+
+
+
+// PROTOTYPES
+void draw_bg(void);
+void draw_sprites(void);
+void movement(void);
+void bg_collision(void);
+void check_start(void);
